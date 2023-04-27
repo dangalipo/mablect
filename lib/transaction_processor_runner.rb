0cbff823ba153be1125848639c3d6ad8458ea439
Dan Galipo <dan.v.galipo@gmail.com>
2023-04-27 15:52:16 +1000

    Linter fixes

--- lib/transaction_processor_runner.rb
+++ lib/transaction_processor_runner.rb
@@ -4,25 +4,20 @@ require_relative 'account_repository'
 require_relative 'account'
 require_relative 'transaction_processor'
 
-
 def run(accounts_filename, transactions_filename)
-
   repo = AccountRepository.new(accounts_filename:)
   processor = TransactionProcessor.new(transactions_filename:)
   begin
     processor.process(repository: repo)
     csv_string = CSV.generate do |csv|
       repo.all.each do |account|
-        csv << [account.account_number, sprintf("%0.02f", (account.balance/100))]
+        csv << [account.account_number, format('%0.02f', (account.balance / 100))]
       end
     end
-    File.write("output.csv",csv_string)
+    File.write('output.csv', csv_string)
   rescue ArgumentError, Account::InsufficientFunds, AccountRepository::AccountNotFound => e
     puts e
   end
-
 end
 
-if $0 == __FILE__
-  run(ARGV[0], ARGV[1])
-end
+run(ARGV[0], ARGV[1]) if $PROGRAM_NAME == __FILE__
--- spec/lib/transaction_processor_runner_spec.rb
+++ spec/lib/transaction_processor_runner_spec.rb
@@ -1,15 +1,17 @@
-require 'csv'
-require_relative "../../lib/transaction_processor_runner"
+# frozen_string_literal: true
 
-describe "run" do
+require 'csv'
+require_relative '../../lib/transaction_processor_runner'
+
+describe 'run' do
   let(:accounts_filename) { 'spec/fixtures/mable_acc_balance.csv' }
-  let(:output_file) { "output.csv" }
+  let(:output_file) { 'output.csv' }
   before do
     File.delete(output_file) if File.exist?(output_file)
   end
   subject(:run_process) { run(accounts_filename, transactions_filename) }
 
-  context "happy path" do
+  context 'happy path' do
     let(:transactions_filename) { 'spec/fixtures/mable_trans.csv' }
     subject(:csv) do
       run_process
@@ -17,38 +19,38 @@ describe "run" do
     end
 
     specify { expect(csv[0][0]).to eq('1111234522226789') }
-    specify { expect(csv[0][1]).to eq("4999.50") }
+    specify { expect(csv[0][1]).to eq('4999.50') }
     specify { expect(csv[1][0]).to eq('3212343433335755') }
     specify { expect(csv[1][1]).to eq('50000.50') }
   end
 
-  context "insufficient funds" do
+  context 'insufficient funds' do
     let(:transactions_filename) { 'spec/fixtures/mable_trans_insufficient_funds.csv' }
 
     specify do
-      expect{run_process}
+      expect { run_process }
         .to output("Widthdrawl of 1000000000.0 from 1111234522226789 would leave account balance negative\n")
-              .to_stdout
+        .to_stdout
     end
   end
 
-  context "negative amounts" do
+  context 'negative amounts' do
     let(:transactions_filename) { 'spec/fixtures/mable_trans_negative_amounts.csv' }
 
     specify do
-      expect{run_process}
+      expect { run_process }
         .to output("Widthdrawl amount must be positive\n")
-              .to_stdout
+        .to_stdout
     end
   end
 
-  context "negative amounts" do
+  context 'negative amounts' do
     let(:transactions_filename) { 'spec/fixtures/mable_trans_missing_account.csv' }
 
     specify do
-      expect{run_process}
+      expect { run_process }
         .to output("Could not find account number 1\n")
-              .to_stdout
+        .to_stdout
     end
   end
 
