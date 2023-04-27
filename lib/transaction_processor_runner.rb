49adb4d762b28abf6a7613f0f0aac1dcf462d362
Dan Galipo <dan.v.galipo@gmail.com>
2023-04-27 15:50:45 +1000

    Added harness for running Transaction processor

---
+++ lib/transaction_processor_runner.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative 'account_repository'
+require_relative 'account'
+require_relative 'transaction_processor'
+
+
+def run(accounts_filename, transactions_filename)
+
+  repo = AccountRepository.new(accounts_filename:)
+  processor = TransactionProcessor.new(transactions_filename:)
+  begin
+    processor.process(repository: repo)
+    csv_string = CSV.generate do |csv|
+      repo.all.each do |account|
+        csv << [account.account_number, sprintf("%0.02f", (account.balance/100))]
+      end
+    end
+    File.write("output.csv",csv_string)
+  rescue ArgumentError, Account::InsufficientFunds, AccountRepository::AccountNotFound => e
+    puts e
+  end
+
+end
+
+if $0 == __FILE__
+  run(ARGV[0], ARGV[1])
+end
